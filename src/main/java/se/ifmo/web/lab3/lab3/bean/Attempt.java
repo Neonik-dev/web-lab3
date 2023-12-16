27f0d91738b0937a18e513e158622b41ac4856ed
neonik <lis.gleb2012@gmail.com>
2023-12-16 19:22:19 +0300

    add method checkHit

--- src/main/java/se/ifmo/web/lab3/lab3/bean/Attempt.java
+++ src/main/java/se/ifmo/web/lab3/lab3/bean/Attempt.java
@@ -28,6 +28,19 @@ public class Attempt {
     }
 
     public boolean checkHit() {
-
+        // Triangle in top-right quadrant
+        if (x >= 0 && y >= 0) {
+            return (x <= r) && (-x + r >= y);
+        }
+        // Circle in bottom-right quadrant
+        if (x >= 0 && y <= 0) {
+            return (x * x + y * y) <= (r * r);
+        }
+        // Rectangle in bottom-left quadrant
+        if (x <= 0 && y <= 0) {
+            return (-y <= r / 2) && (-x <= r);
+        }
+        // Empty in top-left quadrant
+        return false;
     }
 }

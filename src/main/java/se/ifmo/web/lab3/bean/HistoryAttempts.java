52cedfd629469891dea705bbdfc049ea75c1110b
neonik <lis.gleb2012@gmail.com>
2023-12-19 13:32:18 +0300

    done lab

--- src/main/java/se/ifmo/web/lab3/bean/HistoryAttempts.java
+++ src/main/java/se/ifmo/web/lab3/bean/HistoryAttempts.java
@@ -8,13 +8,14 @@ import se.ifmo.web.lab3.persistence.service.AttemptService;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Named("historyAttempts")
 @SessionScoped
 @Data
 public class HistoryAttempts implements Serializable {
-    private List<Attempt> attempts = new ArrayList<>();
+    private List<Attempt> attempts = Collections.synchronizedList(new ArrayList<>());
 
     public HistoryAttempts() {
         new AttemptService().getAttempts(3.5, this);
--- src/main/webapp/scripts/draw.js
+++ src/main/webapp/scripts/draw.js
@@ -1,9 +1,19 @@
-window.addEventListener('DOMContentLoaded', function () {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-    const maxLength = 350;
-    const R = 150
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const maxLength = 350;
+const R = 150
 
+window.onload = () => {
+    createPlot();
+};
+
+function clearPlot() {
+    ctx.clearRect(0, 0, maxLength, maxLength);
+}
+
+
+function createPlot() {
+    clearPlot();
     ctx.fillStyle = "rgba(30, 144, 255, 0.8)";
     drawTopRightQuadrant();
     drawDownLeftQuadrant();
@@ -12,114 +22,123 @@ window.addEventListener('DOMContentLoaded', function () {
     drawArrow(0, maxLength / 2, maxLength, maxLength / 2);
     drawMark()
     ctx.fillStyle = "rgb(0,0,0)";
-    drawText()
+    drawText();
+    setTimeout(() => {
+        iterateAttempts();
+    }, 100)
+}
 
 
-    function drawArrow(fromX, fromY, toX, toY) {
-        ctx.beginPath();
-        const headLen = 10;
-        const dx = toX - fromX;
-        const dy = toY - fromY;
-        const angle = Math.atan2(dy, dx);
-        ctx.moveTo(fromX, fromY);
-        ctx.lineTo(toX, toY);
-        ctx.lineTo(toX - headLen * Math.cos(angle - Math.PI / 6), toY - headLen * Math.sin(angle - Math.PI / 6));
-        ctx.moveTo(toX, toY);
-        ctx.lineTo(toX - headLen * Math.cos(angle + Math.PI / 6), toY - headLen * Math.sin(angle + Math.PI / 6));
-        ctx.stroke();
-    }
+function drawArrow(fromX, fromY, toX, toY) {
+    ctx.beginPath();
+    const headLen = 10;
+    const dx = toX - fromX;
+    const dy = toY - fromY;
+    const angle = Math.atan2(dy, dx);
+    ctx.moveTo(fromX, fromY);
+    ctx.lineTo(toX, toY);
+    ctx.lineTo(toX - headLen * Math.cos(angle - Math.PI / 6), toY - headLen * Math.sin(angle - Math.PI / 6));
+    ctx.moveTo(toX, toY);
+    ctx.lineTo(toX - headLen * Math.cos(angle + Math.PI / 6), toY - headLen * Math.sin(angle + Math.PI / 6));
+    ctx.stroke();
+}
 
-    function drawTopRightQuadrant() {
-        ctx.beginPath();
-        ctx.moveTo(maxLength / 2, maxLength / 2 - R);
-        ctx.lineTo(maxLength / 2, maxLength / 2);
-        ctx.lineTo(maxLength / 2 + R, maxLength / 2);
-        ctx.closePath();
-        ctx.fill();
-    }
+function drawTopRightQuadrant() {
+    ctx.beginPath();
+    ctx.moveTo(maxLength / 2, maxLength / 2 - R);
+    ctx.lineTo(maxLength / 2, maxLength / 2);
+    ctx.lineTo(maxLength / 2 + R, maxLength / 2);
+    ctx.closePath();
+    ctx.fill();
+}
 
-    function drawDownLeftQuadrant() {
-        ctx.beginPath();
-        ctx.fillRect(maxLength / 2 - R, maxLength / 2, R, R / 2);
-        ctx.stroke();
-    }
+function drawDownLeftQuadrant() {
+    ctx.beginPath();
+    ctx.fillRect(maxLength / 2 - R, maxLength / 2, R, R / 2);
+    ctx.stroke();
+}
 
-    function drawDownRightQuadrant() {
-        ctx.beginPath();
-        ctx.arc(maxLength / 2, maxLength / 2, R, 0, Math.PI / 2, false);
-        ctx.lineTo(maxLength / 2, maxLength / 2);
-        ctx.fill();
-    }
+function drawDownRightQuadrant() {
+    ctx.beginPath();
+    ctx.arc(maxLength / 2, maxLength / 2, R, 0, Math.PI / 2, false);
+    ctx.lineTo(maxLength / 2, maxLength / 2);
+    ctx.fill();
+}
 
-    function drawMark() {
-        const lenMark = 5;
-        ctx.beginPath();
-        ctx.moveTo(maxLength / 2 - lenMark, maxLength / 2 - R); // Oy
-        ctx.lineTo(maxLength / 2 + lenMark, maxLength / 2 - R);
+function drawMark() {
+    const lenMark = 5;
+    ctx.beginPath();
+    ctx.moveTo(maxLength / 2 - lenMark, maxLength / 2 - R); // Oy
+    ctx.lineTo(maxLength / 2 + lenMark, maxLength / 2 - R);
 
-        ctx.moveTo(maxLength / 2 - lenMark, maxLength / 2 - R / 2);
-        ctx.lineTo(maxLength / 2 + lenMark, maxLength / 2 - R / 2);
+    ctx.moveTo(maxLength / 2 - lenMark, maxLength / 2 - R / 2);
+    ctx.lineTo(maxLength / 2 + lenMark, maxLength / 2 - R / 2);
 
-        ctx.moveTo(maxLength / 2 - lenMark, maxLength / 2 + R / 2);
-        ctx.lineTo(maxLength / 2 + lenMark, maxLength / 2 + R / 2);
+    ctx.moveTo(maxLength / 2 - lenMark, maxLength / 2 + R / 2);
+    ctx.lineTo(maxLength / 2 + lenMark, maxLength / 2 + R / 2);
 
-        ctx.moveTo(maxLength / 2 - lenMark, maxLength / 2 + R);
-        ctx.lineTo(maxLength / 2 + lenMark, maxLength / 2 + R);
+    ctx.moveTo(maxLength / 2 - lenMark, maxLength / 2 + R);
+    ctx.lineTo(maxLength / 2 + lenMark, maxLength / 2 + R);
 
-        ctx.moveTo(maxLength / 2 - R, maxLength / 2 - lenMark); // Ox
-        ctx.lineTo(maxLength / 2 - R, maxLength / 2 + lenMark);
+    ctx.moveTo(maxLength / 2 - R, maxLength / 2 - lenMark); // Ox
+    ctx.lineTo(maxLength / 2 - R, maxLength / 2 + lenMark);
 
-        ctx.moveTo(maxLength / 2 - R / 2, maxLength / 2 - lenMark);
-        ctx.lineTo(maxLength / 2 - R / 2, maxLength / 2 + lenMark);
+    ctx.moveTo(maxLength / 2 - R / 2, maxLength / 2 - lenMark);
+    ctx.lineTo(maxLength / 2 - R / 2, maxLength / 2 + lenMark);
 
-        ctx.moveTo(maxLength / 2 + R / 2, maxLength / 2 - lenMark);
-        ctx.lineTo(maxLength / 2 + R / 2, maxLength / 2 + lenMark);
+    ctx.moveTo(maxLength / 2 + R / 2, maxLength / 2 - lenMark);
+    ctx.lineTo(maxLength / 2 + R / 2, maxLength / 2 + lenMark);
 
-        ctx.moveTo(maxLength / 2 + R, maxLength / 2 - lenMark);
-        ctx.lineTo(maxLength / 2 + R, maxLength / 2 + lenMark);
+    ctx.moveTo(maxLength / 2 + R, maxLength / 2 - lenMark);
+    ctx.lineTo(maxLength / 2 + R, maxLength / 2 + lenMark);
 
-        ctx.stroke();
-    }
+    ctx.stroke();
+}
 
-    function drawText() {
-        const delta = 10;
-        ctx.font = "17px serif"
-        ctx.fillText("y", maxLength / 2 + delta, delta);
-        ctx.fillText("R", maxLength / 2 + delta, maxLength / 2 - R + delta / 2);
-        ctx.fillText("R/2", maxLength / 2 + delta, maxLength / 2 - R / 2 + delta / 2);
-        ctx.fillText("-R/2", maxLength / 2 + delta, maxLength / 2 + R / 2 + delta / 2);
-        ctx.fillText("-R", maxLength / 2 + delta, maxLength / 2 + R + delta / 2);
+function drawText() {
+    const delta = 10;
+    ctx.font = "17px serif"
+    ctx.fillText("y", maxLength / 2 + delta, delta);
+    ctx.fillText("R", maxLength / 2 + delta, maxLength / 2 - R + delta / 2);
+    ctx.fillText("R/2", maxLength / 2 + delta, maxLength / 2 - R / 2 + delta / 2);
+    ctx.fillText("-R/2", maxLength / 2 + delta, maxLength / 2 + R / 2 + delta / 2);
+    ctx.fillText("-R", maxLength / 2 + delta, maxLength / 2 + R + delta / 2);
 
-        ctx.fillText("x", maxLength - delta, maxLength / 2 - delta);
-        ctx.fillText("-R", maxLength / 2 - R - delta, maxLength / 2 - delta);
-        ctx.fillText("-R/2", maxLength / 2 - R / 2 - delta, maxLength / 2 - delta);
-        ctx.fillText("R/2", maxLength / 2 + R / 2 - delta, maxLength / 2 - delta);
-        ctx.fillText("R", maxLength / 2 + R - delta / 2, maxLength / 2 - delta);
-    }
+    ctx.fillText("x", maxLength - delta, maxLength / 2 - delta);
+    ctx.fillText("-R", maxLength / 2 - R - delta, maxLength / 2 - delta);
+    ctx.fillText("-R/2", maxLength / 2 - R / 2 - delta, maxLength / 2 - delta);
+    ctx.fillText("R/2", maxLength / 2 + R / 2 - delta, maxLength / 2 - delta);
+    ctx.fillText("R", maxLength / 2 + R - delta / 2, maxLength / 2 - delta);
+}
 
-    function drawPoint(x, y, r) {
-        ctx.fillRect(maxLength / 2 + R * (x / r) - 1, maxLength / 2 - R * (y / r) - 1, 2, 2);
-    }
+canvas.onclick = (event) => {
+    const userR = getR();
 
-    canvas.onclick = (event) => {
-        const userR = getR();
+    const x = event.pageX - event.target.offsetLeft - maxLength / 2;
+    const y = maxLength / 2 - event.pageY + event.target.offsetTop;
 
-        const x = event.pageX - event.target.offsetLeft - maxLength / 2;
-        const y = maxLength / 2 - event.pageY + event.target.offsetTop;
+    setY(Math.round(Math.round(y / R * userR)));
+    document.getElementById("sendToServer:X-input_hinput").value = Math.round(x / R * userR * 1000) / 1000;
+    document.getElementById("sendToServer:X-input_input").value = Math.round(x / R * userR * 1000) / 1000;
+    document.getElementById("sendToServer:rValue").value = userR;
+    document.getElementById('sendToServer:sendFormButton').click();
+}
 
-        setY(Math.round(Math.round(y / R * userR)));
-        document.getElementById("sendToServer:X-input_hinput").value = Math.round(x / R * userR * 1000) / 1000;
-        document.getElementById("sendToServer:X-input_input").value = Math.round(x / R * userR * 1000) / 1000;
-        document.getElementById("sendToServer:rValue").value = userR;
-        document.getElementById('sendToServer:sendFormButton').click();
-    }
-
-    function iterateAttempts() {
+function iterateAttempts() {
+    if (document.getElementById("generateTable") != null) {
         const arr = document.getElementById("generateTable").getElementsByTagName("tbody")[0].innerText.split("\n");
         for (let i = 0; i < arr.length; i++) {
             let attempt = arr[i].split("\t");
-            drawPoint(attempt[0], attempt[1], attempt[2]);
+            drawPoint(attempt[0], attempt[1], attempt[2], attempt[3]);
         }
     }
-    iterateAttempts();
-}, false);
+}
+
+function drawPoint(x, y, r, hit) {
+    if (hit === "true") {
+        ctx.fillStyle = "rgb(6,236,17)";
+    } else {
+        ctx.fillStyle = "rgb(245,6,6)";
+    }
+    ctx.fillRect(maxLength / 2 + R * (x / r) - 1, maxLength / 2 - R * (y / r) - 1, 2, 2);
+}
